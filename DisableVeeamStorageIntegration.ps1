f0ae8e71dbe07f727fe70d154769b976259dde3b
knalbone <kmnalbone@gmail.com>
2017-02-15 14:45:18 -0500

    Added basic scripts

---
+++ DisableVeeamStorageIntegration.ps1
@@ -0,0 +1,7 @@
+﻿Add-PSSnapin VeeamPSSnapin
+$jobs = Get-VBRJob
+foreach ($job in $jobs){
+$options = $job.getoptions()
+$options.SanIntegrationOptions.UseSanSnapshots = $False
+Set-VBRJobOptions -Job $job -Options $options
+}
